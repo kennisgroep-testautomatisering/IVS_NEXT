c2cee83215c303bcbf02beef5f38e0d87f84cedd
kennisgroep-testautomatisering <31278022+kennisgroep-testautomatisering@users.noreply.github.com>
2018-12-17 16:01:56 +0100

    Add files via upload

--- aangepaste_versie/page.py
+++ aangepaste_versie/page.py
@@ -13,10 +13,10 @@ class BasePage(object):
 
 class MainPage(BasePage):
     def login(self, username, password):
-        WAIT = 1
+        WAIT = 30
         wait = WebDriverWait(self.driver, WAIT)
         logging.info("Going to login")
-        locator_login = (By.CSS_SELECTOR, "#logn-button")
+        locator_login = (By.CSS_SELECTOR, "#login-button")
         elem = wait.until(EC.presence_of_element_located(locator_login))
         #elem = wait.until(EC.element_to_be_clickable(locator_login))
         elem.click()
